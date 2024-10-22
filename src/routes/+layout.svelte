b888e587be062046945518bb74f2154b74a1b845
sergio <riosgenuziosergio@gmail.com>
2024-10-21 23:20:44 -0400

    css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import "../app.css";
+	import {Navbar} from '$lib'
+</script>
+
+
+<Navbar />
+<slot />
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: ['nord']
+  }
+}
